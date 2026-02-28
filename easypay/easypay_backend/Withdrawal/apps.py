24f3ebdc80f91b9084e1ff706c9a1dc04a86c252
Reagan-dev <reagansimiyu38@gmail.com>
2026-02-28 12:20:22 +0300

    fixed apps.py error

--- easypay/easypay_backend/Withdrawal/apps.py
+++ easypay/easypay_backend/Withdrawal/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class WithdrawalConfig(AppConfig):
-    name = 'Withdrawal'
+    name = 'withdrawal'
     def ready(self):
-        import Withdrawal.signals  # Import signals to ensure they are registered when the app is ready
+        import withdrawal.signals  # Import signals to ensure they are registered when the app is ready
